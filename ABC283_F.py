e9f5996bd33532ea232889ac78c082746cf37640
katonyonko <katonyonko@yahoo.co.jp>
2023-01-03 23:10:46 +0900

    Pythonは細かいソートの回数でも実行時間に効きそう

--- ABC283_F.py
+++ ABC283_F.py
@@ -14,4 +14,72 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 case_no=int(input())
 for __ in range(case_no):
-  
+
+  class SegTree:
+    X_unit = 10**6
+    X_f = min
+
+    def __init__(self, N):
+      self.N = N
+      self.X = [self.X_unit] * (N + N)
+
+    def build(self, seq):
+      for i, x in enumerate(seq, self.N):
+        self.X[i] = x
+      for i in range(self.N - 1, 0, -1):
+        self.X[i] = self.X_f(self.X[i << 1], self.X[i << 1 | 1])
+
+    def set_val(self, i, x):
+      i += self.N
+      self.X[i] = x
+      while i > 1:
+        i >>= 1
+        self.X[i] = self.X_f(self.X[i << 1], self.X[i << 1 | 1])
+
+    def fold(self, L, R):
+      L += self.N
+      R += self.N
+      vL = self.X_unit
+      vR = self.X_unit
+      while L < R:
+        if L & 1:
+          vL = self.X_f(vL, self.X[L])
+          L += 1
+        if R & 1:
+          R -= 1
+          vR = self.X_f(self.X[R], vR)
+        L >>= 1
+        R >>= 1
+      return self.X_f(vL, vR)
+
+  N=int(input())
+  P=list(map(lambda x: int(x)-1,input().split()))
+  ans=[10**6]*N
+
+  tmp=sorted([(P[i],i) for i in range(N)], reverse=True)
+  st=SegTree(N)
+  for i in range(N):
+    y,x=tmp[i]
+    st.set_val(x,x+y)
+    ans[x]=min(ans[x],st.fold(x+1,N)-x-y)
+
+  st=SegTree(N)
+  for i in range(N):
+    y,x=tmp[i]
+    st.set_val(x,-x+y)
+    ans[x]=min(ans[x],st.fold(0,x)+x-y)
+
+  tmp=sorted([(P[i],i) for i in range(N)])
+  st=SegTree(N)
+  for i in range(N):
+    y,x=tmp[i]
+    st.set_val(x,-x-y)
+    ans[x]=min(ans[x],st.fold(0,x)+x+y)
+
+  st=SegTree(N)
+  for i in range(N):
+    y,x=tmp[i]
+    st.set_val(x,x-y)
+    ans[x]=min(ans[x],st.fold(x+1,N)-x+y)
+
+  print(*ans)
